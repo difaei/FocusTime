6757cb76f5f69f2e83b6a285751b3b1cee74494b
Simo Jamal <simojamal15@gmail.com>
2022-07-29 16:40:33 +0400

    initial

---
+++ src/features/Timing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { RoundedButton } from '../components/roundedButton';
+
+export const Timing = ({ onChangeTime }) => {
+  return (
+    <>
+      <View style={styles.timing}>
+        <RoundedButton title="10" size={75} onPress={() => onChangeTime(10)} />
+      </View>
+      <View style={styles.timing}>
+        <RoundedButton title="15" size={75} onPress={() => onChangeTime(15)} />
+      </View>
+      <View style={styles.timing}>
+        <RoundedButton title="20" size={75} onPress={() => onChangeTime(20)} />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  timing: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
